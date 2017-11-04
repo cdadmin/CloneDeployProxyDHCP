ea9f69c644f0126f35e8bb3e7a408727ae53a4ac
cdadmin <crucibleadmin@cruciblewds.org>
2017-11-04 00:17:16 -0400

    update version

--- Proxy_Dhcp/Program.cs
+++ Proxy_Dhcp/Program.cs
@@ -62,7 +62,7 @@ namespace CloneDeploy_Proxy_Dhcp
 
             if (args.Length > 0 && ContainsSwitch(args, "version"))
             {
-                Console.WriteLine("2.1.1");
+                Console.WriteLine("2.1.2");
                 Environment.Exit(0);
             }
 
